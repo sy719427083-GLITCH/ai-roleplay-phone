25cd91dd9d20fd67a749cffff7a8c4acaaa8f374
sy719427083-GLITCH <719427083@qq.com>
2026-06-27 23:55:20 +0800

    Reduce phone top spacing

--- src/App.jsx
+++ src/App.jsx
@@ -282,7 +282,7 @@ function SettingsScreen({ onOpen }) {
           );
         })}
       </div>
-      <p className="version-label">Ccat OS v0.1.20</p>
+      <p className="version-label">Ccat OS v0.1.21</p>
     </section>
   );
 }
