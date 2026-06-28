7964f32584257e73657ed908aff5dc348473a8ce
sy719427083-GLITCH <719427083@qq.com>
2026-06-28 17:10:30 +0800

    Tighten app top spacing

--- src/App.jsx
+++ src/App.jsx
@@ -282,7 +282,7 @@ function SettingsScreen({ onOpen }) {
           );
         })}
       </div>
-      <p className="version-label">Ccat OS v0.1.22</p>
+      <p className="version-label">Ccat OS v0.1.23</p>
     </section>
   );
 }
